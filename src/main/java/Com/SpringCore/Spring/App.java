245391d3de64e9e219de007bf76d28fb95d0a570
Akshaysarsamkar <sarsamkarakshay@gmail.com>
2024-07-21 01:01:24 -0700

    ADD ANOTHER EXAMPLE

--- src/main/java/Com/SpringCore/Spring/App.java
+++ src/main/java/Com/SpringCore/Spring/App.java
@@ -13,10 +13,16 @@ public class App
     {
         System.out.println( "Hello World!" );
         
-      ApplicationContext context =   new ClassPathXmlApplicationContext("config.xml");
-     Collage c = (Collage) context.getBean("col");
-     System.out.println(c);
-     System.out.println(c.getCname());
-     
+//      ApplicationContext context =   new ClassPathXmlApplicationContext("config.xml");
+//     Collage c = (Collage) context.getBean("col");
+//     System.out.println(c);
+//     System.out.println(c.getCname());
+        
+        
+        ApplicationContext c = new ClassPathXmlApplicationContext("config.xml");
+        City city = (City) c.getBean("CityData");
+        System.out.println(city);
+        
+        System.out.println("Population of the " + city.getCityName() + " is:" + city.getPopulation());
     }
 }
---
+++ src/main/java/Com/SpringCore/Spring/City.java
@@ -0,0 +1,55 @@
+package Com.SpringCore.Spring;
+
+//the class which containes setter,getter and no logic will be their is known as pojo class  
+
+
+public class City {
+
+	private int cid;
+	private String CityName;
+	private String State;
+	private int population;
+	public int getCid() {
+		return cid;
+	}
+	public void setCid(int cid) {
+		this.cid = cid;
+	}
+	public String getCityName() {
+		return CityName;
+	}
+	public void setCityName(String cityName) {
+		CityName = cityName;
+	}
+	public String getState() {
+		return State;
+	}
+	public void setState(String state) {
+		State = state;
+	}
+	public int getPopulation() {
+		return population;
+	}
+	public void setPopulation(int population) {
+		this.population = population;
+	}
+	
+	public City(int cid, String cityName, String state, int population) {
+		super();
+		this.cid = cid;
+		CityName = cityName;
+		State = state;
+		this.population = population;
+	}
+	public City() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	@Override
+	public String toString() {
+		return "City [cid=" + cid + ", CityName=" + CityName + ", State=" + State + ", population=" + population + "]";
+	}
+
+	
+}
