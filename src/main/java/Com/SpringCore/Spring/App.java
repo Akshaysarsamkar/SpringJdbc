2f05de1deb862317e530dc39f3fa935a018e263d
Akshaysarsamkar <sarsamkarakshay@gmail.com>
2024-07-21 00:41:58 -0700

    First Spring Program

---
+++ src/main/java/Com/SpringCore/Spring/App.java
@@ -0,0 +1,22 @@
+package Com.SpringCore.Spring;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+      ApplicationContext context =   new ClassPathXmlApplicationContext("config.xml");
+     Collage c = (Collage) context.getBean("col");
+     System.out.println(c);
+     System.out.println(c.getCname());
+     
+    }
+}
